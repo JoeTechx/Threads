d47ea6d8c7e1c3e637413510a4b8e0e975b868f8
JOEX <oyinjoe23@gmail.com>
2023-10-10 14:02:50 +0100

    feat: Search UserCard

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,4 +1,20 @@
-const Page = () => {
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  // fetch organization list created by user
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  // Convert userInfo._id to a string if needed
+  //   const userId = userInfo._id.toString();
+
+// getActivity
+
     return (
       <section>
         <h1 className="head-text mb-10">Activity</h1>
