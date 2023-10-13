3bcf6cef8ef7c826e282cf5b6451d9408a1ea4ec
JOEX <oyinjoe23@gmail.com>
2023-10-14 00:55:51 +0100

    updated root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,7 +8,7 @@ import { Inter } from "next/font/google"
 import '../globals.css'
 export const metadata = {
   title: 'Threads',
-  description: 'A Next.js 13 Meta Threads Application'
+  description: 'Threads Application'
 }
 
 const inter = Inter ({ subsets: ["latin"] })
